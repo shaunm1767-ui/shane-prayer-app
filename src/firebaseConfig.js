a711fbdd06fc3fa443e6207ece7c34f3f8cb4efc
shaunm1767-ui <shaunm1767@gmail.com>
2026-03-24 17:03:02 +0200

    Initial commit: Firebase setup + hosting live

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyAPbv7QREu9B7AAQ-_wNTVeMpLPrZWp88M",
+  authDomain: "satsang-moments.firebaseapp.com",
+  projectId: "satsang-moments",
+  storageBucket: "satsang-moments.firebasestorage.app",
+  messagingSenderId: "480844021527",
+  appId: "1:480844021527:web:ed638730391a5d34cfc152"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);

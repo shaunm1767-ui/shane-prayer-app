4109a68e8262225676b4c89c421af66bda884322
shaunm1767-ui <shaunm1767@gmail.com>
2026-03-24 19:27:34 +0200

    Keep only mp3 files in Git LFS, remove mp4 files from repo

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAPbv7QREu9B7AAQ-_wNTVeMpLPrZWp88M",
   authDomain: "satsang-moments.firebaseapp.com",
   projectId: "satsang-moments",
-  storageBucket: "satsang-moments.firebasestorage.app",
+  storageBucket: "satsang-moments.appspot.com",
   messagingSenderId: "480844021527",
   appId: "1:480844021527:web:ed638730391a5d34cfc152"
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);

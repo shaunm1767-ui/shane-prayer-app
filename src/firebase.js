2a8a99f1f7123c6b52dc3989b0169bcb33314cf7
shaunm1767-ui <shaunm1767@gmail.com>
2026-04-07 14:58:31 +0200

    AUTO PLAYLIST SYSTEM: Firebase storage scanner enabled

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+﻿import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCiaayvLYeVvy1WjA1HMwncgUFlNBgzRT0",
-  authDomain: "shane-prayer-app-2026.firebaseapp.com",
-  projectId: "shane-prayer-app-2026",
-  storageBucket: "shane-prayer-app-2026.firebasestorage.app",
-  messagingSenderId: "608379916251",
-  appId: "1:608379916251:web:fc40ea4a810de78d3121fe"
+  apiKey: "AIzaSyBrtiWYAhVd4lx3gyY3RtJk0GEn7MotRJI",
+  authDomain: "shane-storage-90931.firebaseapp.com",
+  projectId: "shane-storage-90931",
+  storageBucket: "shane-storage-90931.firebasestorage.app",
+  messagingSenderId: "321117848066",
+  appId: "1:321117848066:web:09e74895b8ecec3c475d1b"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const storage = getStorage(app);
---
+++ src/utils/loadPlaylist.js
@@ -0,0 +1,21 @@
+﻿import { storage } from "../firebase";
+import { ref, listAll, getDownloadURL } from "firebase/storage";
+
+export async function loadPlaylist() {
+  const folderRef = ref(storage, "/");
+
+  const result = await listAll(folderRef);
+
+  const tracks = await Promise.all(
+    result.items.map(async (item) => {
+      const url = await getDownloadURL(item);
+
+      return {
+        title: item.name.replace(/\.[^/.]+$/, ""),
+        url
+      };
+    })
+  );
+
+  return tracks;
+}

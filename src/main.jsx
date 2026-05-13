7751fc69df57f658c4e4a4eb80c8d92efc5c8a0f
shaunm1767-ui <shaunm1767@gmail.com>
2026-05-13 16:18:48 +0200

    CHECKPOINT: stable audio engine + playlist working + UI loads

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AudioProvider } from "./context/AudioProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AudioProvider>
+  <React.StrictMode>
     <App />
-  </AudioProvider>
+  </React.StrictMode>
 );

8d87d6335bf5cda935634652e943055746dc6039
shaunm1767-ui <shaunm1767@gmail.com>
2026-06-08 18:16:29 +0200

    backup before switching to AppShell architecture

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,12 @@
 import React, { useContext } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
 import Login from "./Login";
-
-import Player from "./screens/Player";
-import Home from "./screens/HomeScreen";
+import AppShell from "./layout/AppShell";
 import { AuthContext } from "./AuthProvider";
 
 export default function App() {
   const { user } = useContext(AuthContext);
 
-  if (!user) {
-    return <Login />;
-  }
+  if (!user) return <Login />;
 
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/player" element={<Player />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  );
+  return <AppShell />;
 }
